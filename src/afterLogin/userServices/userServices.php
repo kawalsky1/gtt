e9d64bea6b394dcbfbc8ead4d65988bfd0d8e5fe
Costa <ncostantini@outlook.com>
2022-06-25 20:02:09 +0200

    removing space

--- src/afterLogin/userServices/userServices.php
+++ src/afterLogin/userServices/userServices.php
@@ -241,7 +241,7 @@ require "../sessions/userSession.php";
   ?>
 </div>
 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br>
 <!-- Footer -->
 <footer style="width:101%; background-color: black; color:white;" class="w3-container w3-padding-64 w3-center w3-opacity">  
   <div class="w3-xlarge w3-padding-32">
